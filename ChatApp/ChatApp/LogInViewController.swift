03350a15d0344254e8e902b9347dbf1f8a332882
Hannah Joyce Villasin <hvillasin@ucmerced.edu>
2021-04-12 20:11:57 -0700

    create account, log in

---
+++ ChatApp/ChatApp/LogInViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LogInViewController.swift
+//  ChatApp
+//
+//  Created by Hannah Villasin on 4/8/21.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
